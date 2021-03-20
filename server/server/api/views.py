24386dbcec7f88cc1658af707f1acc59aaa26aa5
Tanner Collin <git@tannercollin.com>
2021-03-20 16:01:10 -0600

    Set up serializer

--- server/server/api/views.py
+++ server/server/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from server.api import serializers
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = serializers.UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
