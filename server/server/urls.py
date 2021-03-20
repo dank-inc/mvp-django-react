1ee7ca25e1a1ce80e8a279c20b9dfb8d58d277ae
Tanner Collin <git@tannercollin.com>
2021-03-20 16:07:43 -0600

    Install rest-auth

--- server/server/urls.py
+++ server/server/urls.py
@@ -7,5 +7,7 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 ]
