56d3a1a862479ad16e50797e40887b6aa55c62b8
Tanner Collin <git@tannercollin.com>
2021-03-20 17:11:47 -0600

    Add Django admin site

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls import url
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from .api import views
@@ -7,6 +9,7 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
